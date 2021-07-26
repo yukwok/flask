5fb24bf163161aa6b3d017f1d1f49831a6ac7e0f
Yukwok <billynyk@gmail.com>
2021-07-26 22:45:44 +0800

    testing server

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 # --- initial comment
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/hello")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+# $ export FLASK_APP=hello
+# $ flask run
+#  * Running on http://127.0.0.1:5000/
+
+# /Users/prog/Library/Python/3.8/bin
