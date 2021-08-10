ef79d5052fa543a22ce0a2cf6b8ee4d3d70b2d4b
yukwok <billynyk@gmail.com>
2021-08-10 15:37:40 +0800

    Update main.py
    variable route

--- main.py
+++ main.py
@@ -3,10 +3,22 @@ from flask import Flask
 
 app = Flask(__name__)
 
+print(__name__)
+
 
 @app.route("/hello")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, World!...</p>"
+
+
+@app.route("/")
+def in_homepage():
+    return "<p>Hello, World! i am in root.</p>"
+
+
+@app.route("/username/<name>/<int:age>")
+def age(name, age):
+    return f"<p>Hello, World! i am {name} in {age} years old.</p>"
 
 
 # $ export FLASK_APP=hello
@@ -15,4 +27,5 @@ def hello_world():
 
 # /Users/prog/Library/Python/3.8/bin
 
-# dummy dummy dummy
+if __name__ == "__main__":
+    app.run(debug=True)
