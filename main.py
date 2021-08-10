9bf787fb3f023ae726a7ec7a8c7feac4b97a7304
yukwok <billynyk@gmail.com>
2021-08-10 16:57:38 +0800

    Update main.py
    final number 1 to 9 guessing game

--- main.py
+++ main.py
@@ -1,24 +1,52 @@
 # --- initial comment
 from flask import Flask
 
+import random
+
+random_number = random.randint(0, 9)
+print(random_number)
+
+
 app = Flask(__name__)
 
-# print(__name__)
+print(__name__)
 
 
-@app.route("/hello")
-def hello_world():
-    return "<p>Hello, World!...</p>"
+@app.route('/')
+def home():
+    return "<h1>Guess a number between 0 and 9</h1>" \
+           "<img src='https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif'/>"
 
 
-@app.route("/")
-def in_homepage():
-    return "<p>Hello, World! i am in root.</p>"
+@app.route("/<int:guess>")
+def guess_number(guess):
+    if guess > random_number:
+        return "<h1 style='color: purple'>Too high, try again!</h1>" \
+               "<img src='https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif'/>"
+
+    elif guess < random_number:
+        return "<h1 style='color: red'>Too low, try again!</h1>"\
+               "<img src='https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif'/>"
+    else:
+        return "<h1 style='color: green'>You found me!</h1>" \
+               "<img src='https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif'/>"
 
 
-@app.route("/username/<name>/<int:age>")
-def age(name, age):
-    return f"<p>Hello, World! i am {name} in {age} years old.</p>"
+# @app.route("/hello")
+# def hello_world():
+#     return "<p>Hello, World!...</p>"
+
+
+# @app.route("/")
+# def in_homepage():
+#     return "<p>Hello, World! i am in root.</p>"
+
+#     # sdgdsgdsfg
+
+
+# @app.route("/username/<name>/<int:age>")
+# def age(name, age):
+#     return f"<p>Hello, World! i am {name} in {age} years old.</p>"
 
 
 # $ export FLASK_APP=hello
@@ -29,3 +57,6 @@ def age(name, age):
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+# comments at the end
